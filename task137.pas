6597c247df4b8cc8adc621181a5fbb263393a35a
Hexi14 <92674749+Hexi14@users.noreply.github.com>
2022-03-17 00:24:02 +0900

    Add files via upload

---
+++ task137.pas
@@ -0,0 +1,71 @@
+﻿program task137;
+uses crt;
+
+function fact(n: integer): longint;
+begin
+  if (n = 0) then
+    fact:=1
+  else
+    fact :=n *fact(n-1);
+end;
+
+var
+  n:integer;
+  a_array: array of real;
+  i:integer; j:integer;
+  a:real;
+  
+begin
+ writeln('Enter n: '); readln(n);
+ setlength(a_array,n);
+ 
+ 
+ for i:=0 to n-1 do
+ begin
+   writeln('Enter A[',i,']: '); readln(a_array[i]);
+ end;
+
+(* a)   *)
+ for i:=0 to n-1 do
+   a := a + a_array[i];
+
+  writeln(a_array);
+  writeln('a) ',a);
+  
+(* b)  *)
+a := a_array[0] * a_array[0];
+
+for i:=1 to n-1 do
+  a := a * a_array[i];
+
+
+writeln('b) ',a);
+
+(* v)  *)
+a := Abs(a_array[0]);
+for i:=1 to n-1 do
+  a := a + Abs(a_array[i]);
+
+writeln('v) ',a);
+
+(* g)  *)
+a := a_array[0];
+for i:=1 to n-1 do
+  a := ((-1)**(n+1)*a)*a_array[i];
+
+writeln('g) ',a);
+(* d) *)
+a := a_array[0] * (-1);
+for i:=1 to n-1 do
+  a := ((-1)**i)*a_array[i];
+writeln('d) ',a);
+(* e) *)
+a := 0;
+for i:=0 to n-1 do
+  a := a_array[i] + fact(i);
+writeln('e) ',a);
+
+     
+  
+
+end.
