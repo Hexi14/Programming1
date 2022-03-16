41ce41f93819fa31c1d0dacb396b925cae11512f
Bxcker <95162181+Bxcker@users.noreply.github.com>
2022-03-16 23:35:32 +0900

    Add files via upload

---
+++ task58g.pas
@@ -0,0 +1,10 @@
+﻿program gfd44;
+var
+  x,y:real;
+begin
+   write('x = '); readln(x);
+   if(x<0)then y:=-x
+   else if(0<=x) and (x<=1)then y:=x
+   else if(2<x) then y:=-2*x+5;
+   writeln('y=',y);
+end.
