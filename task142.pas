95864a94541d5fd750c1bed03238f97ca8fe971b
Bxcker <95162181+Bxcker@users.noreply.github.com>
2022-03-18 09:02:35 +0900

    Add files via upload

---
+++ task142.pas
@@ -0,0 +1,18 @@
+﻿program task142;
+const
+  n = 5;
+ 
+var
+  a: array[0.. n] of Double := (-9.6, 0, 1.7, 0, -9, 1);  
+  f: Double;
+  i, x: integer;
+ 
+begin
+  for x := 0 to 5 do
+  begin
+    f := a[n];
+    for i := n - 1 downto 0 do
+      f := f * x + a[i];
+    writeln('x = ', x, ' ; f(x) = ', f:0:5);
+  end;
+end.
---
+++ task43.pas
@@ -0,0 +1,15 @@
+﻿program task43;
+var
+  a,b,c:real;
+  
+begin
+write ('Введите a= '); readln(a);
+  if a>=0 then a:=sqr(a) else a:=0;
+write ('Введите b= '); readln(b);
+  if b>=0 then b:=sqr(b) else b:=0;
+write ('Введите c= '); readln(c);
+  if c>=0 then c:=sqr(c) else c:=0;
+
+writeln('a=',a,' b=',b,' c=',c); 
+end.
+
